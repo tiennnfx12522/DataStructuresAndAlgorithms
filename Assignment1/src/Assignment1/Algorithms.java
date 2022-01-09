1802153664222d7f963b51cd2aea6db342013eb4
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2022-01-10 01:45:03 +0900

    Add files via upload

--- Assignment1/src/Assignment1/Algorithms.java
+++ Assignment1/src/Assignment1/Algorithms.java
@@ -1,14 +1,10 @@
 package Assignment1;
-import javax.sound.midi.Soundbank;
 import java.io.*;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Algorithms {
-
-    public void writeFile(String fileName, float [] arr) throws IOException {
+    public void writeFile(String fileName, float [] arr) {
         try {
             File file = new File(fileName);
             FileOutputStream fos = new FileOutputStream(file);
@@ -22,8 +18,34 @@ public class Algorithms {
         }
     }
 
-    public float[] readFile(String fileName) {
-        List<Float> list = new ArrayList<Float>();
+    public void writeFile(String fileName, int [] arr) {
+        try {
+            File file = new File(fileName);
+            FileOutputStream fos = new FileOutputStream(file);
+            DataOutputStream dos = new DataOutputStream(fos);
+            for (int v : arr) {
+                dos.writeFloat(v);
+            }
+            dos.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void writeFile(String fileName, int value) {
+        try {
+            File file = new File(fileName);
+            FileOutputStream fos = new FileOutputStream(file);
+            DataOutputStream dos = new DataOutputStream(fos);
+            dos.writeInt(value);
+            dos.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    float[] readFile(String fileName) {
+        List<Float> list = new ArrayList<>();
         try {
             FileInputStream fis = new FileInputStream(fileName);
             DataInputStream dis = new DataInputStream(fis);
@@ -32,41 +54,123 @@ public class Algorithms {
             }
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            float [] arr = new float [list.size()];
-            for (int i = 0; i < list.size(); i++) {
-                arr[i] = list.get(i);
-            }
-            System.out.print("Array a : ");
-            for (float v : arr) {
-                System.out.print(v + "  ");
-            }
-            System.out.println(" ");
-            return arr;
         }
+        float [] arr = new float [list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
     }
 
-    public float[] bubbleSort(float [] b) {
+    void bubbleSort(float [] b) {
         boolean swappedSomething;
         do {
             swappedSomething = false;
             for (int i = 0; i < b.length - 1; i++) {
                 if (b[i] > b[i + 1]) {
                     swappedSomething = true;
-                    float temp = b[i];
-                    b[i] = b[i + 1];
-                    b[i + 1] = temp;
+                    swap(b, i, i + 1);
                 }
             }
             printArray(b);
             System.out.println(" ");
         } while (swappedSomething);
+    }
+
+    void selectionSort(float [] b) {
+        for (int i = 0; i < b.length - 1; i++) {
+            int minIndex = i;
+            float minValue = b[i];
+            for (int j = i; j < b.length; j++) {
+                if (b[j] < minValue) {
+                    minIndex = j;
+                    minValue = b[j];
+                }
+            }
+            if (minValue < b[i]) {
+                swap(b, i, minIndex);
+            }
+            printArray(b);
+            System.out.println(" ");
+        }
+    }
+
+    void insertionSort(float [] b) {
+        for (int i = 1; i < b.length; i++) {
+            float key = b[i];
+            int j = i - 1;
+            while (j >= 0 && key < b[j]) {
+                swap(b, j, j + 1);
+                j--;
+            }
+            printArray(b);
+            System.out.println(" ");
+        }
+    }
+
+    float [] insertionSort2(float [] b) {
+        for (int i = 1; i < b.length; i++) {
+            float key = b[i];
+            int j = i - 1;
+            while (j >= 0 && key < b[j]) {
+                swap(b, j, j + 1);
+                j--;
+            }
+        }
         return b;
     }
 
-    public static void printArray (float [] array) {
+    int [] search(float [] b, float value) {
+        int count = 0;
+        List<Integer> list = new ArrayList<>();
+        System.out.print("Index: ");
+        for (int i = 0; i < b.length; i++) {
+            if (b[i] > value) {
+                System.out.print(i + " ");
+                list.add(i);
+            }
+        }
+        int [] output = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            output[i] = list.get(i);
+        }
+        return output;
+    }
+
+    int binarySearch(float arr[], int left, int right, float value) {
+        {
+            if (right >= left) {
+                int mid = left + (right - left) / 2;
+
+                // If the element is present at the middle itself
+                if (arr[mid] == value)
+                    return mid;
+
+                // If element is smaller than mid, then
+                // it can only be present in left sub-array
+                if (arr[mid] > value)
+                    return binarySearch(arr, left, mid - 1, value);
+
+                // Else the element can only be present in right sub-array
+                return binarySearch(arr, mid + 1, right, value);
+            }
+
+            // We reach here when element is not present in array
+            return -1;
+        }
+    }
+
+    void printArray (float [] array) {
         for (float i: array) {
             System.out.print(i + " ");
         }
+    }
+
+    // Swap element at position i with element at position k
+    void swap(float[] a, int i, int k) {
+        float x;
+        x = a[i];
+        a[i] = a[k];
+        a[k] = x;
     }
 }
--- Assignment1/src/Assignment1/mainProgram.java
+++ Assignment1/src/Assignment1/mainProgram.java
@@ -1,26 +1,26 @@
 package Assignment1;
 
-import java.io.*;
 import java.util.Scanner;
 
 public class mainProgram {
     public static final Scanner sc = new Scanner(System.in);
-    public static void main (String[] args) throws IOException {
+    public static void main (String[] args) {
         switchMenu();
     }
 
-    public static void switchMenu () throws IOException {
+    public static void switchMenu () {
         Algorithms t = new Algorithms();
         while(true) {
-            System.out.println("\nChoose your option:");
-            System.out.println("1. Input");
-            System.out.println("2. Output");
-            System.out.println("3. Bubble sort");
-            System.out.println("4. Selection sort");
-            System.out.println("5. Insertion sort");
-            System.out.println("6. Search > value");
-            System.out.println("7. Search = value");
-            System.out.println("0. Exit\n");
+            System.out.println("\n+-------------------Menu--------------------+");
+            System.out.println("|      1.Input                              |");
+            System.out.println("|      2.Out                                |");
+            System.out.println("|      3.Bubble sort                        |");
+            System.out.println("|      4.Selection sort                     |");
+            System.out.println("|      5.Insertion sort                     |");
+            System.out.println("|      6.Search > value                     |");
+            System.out.println("|      7.Search = value                     |");
+            System.out.println("|      0.Exit                               |");
+            System.out.println("+-------------------------------------------+\n");
             System.out.print("Your selection (0 -> 7): ");
             int choice = sc.nextInt();
 
@@ -29,32 +29,48 @@ public class mainProgram {
                 break;
             }
 
-            switch(choice) {
-                case 1:
-                    float [] input = createArray();
+            switch (choice) {
+                case 1 -> {
+                    float[] input = createArray();
                     t.writeFile("INPUT.TXT", input);
-                    break;
-                case 2:
-                    t.readFile("INPUT.TXT");
-                    break;
-                case 3:
-                    float [] b = t.readFile("INPUT.TXT");
+                }
+                case 2 -> {
+                    float[] a = t.readFile("INPUT.TXT");
+                    System.out.print("Array a: ");
+                    t.printArray(a);
+                }
+                case 3 -> {
+                    float[] b = t.readFile("INPUT.TXT");
                     t.bubbleSort(b);
-                    break;
-                case 4:
-
-                    break;
-                case 5:
-
-                    break;
-                case 6:
-
-                    break;
-                case 7:
-
-                    break;
-                default:
-                    System.out.println("**Invalid choice. Try again.**");
+                    t.writeFile("OUTPUT1.TXT", b);
+                }
+                case 4 -> {
+                    float[] c = t.readFile("INPUT.TXT");
+                    t.selectionSort(c);
+                    t.writeFile("OUTPUT2.TXT", c);
+                }
+                case 5 -> {
+                    float[] d = t.readFile("INPUT.TXT");
+                    t.insertionSort(d);
+                    t.writeFile("OUTPUT3.TXT", d);
+                }
+                case 6 -> {
+                    System.out.print("Input value: ");
+                    float value = sc.nextFloat();
+                    float[] e = t.readFile("INPUT.TXT");
+                    int[] output = t.search(e, value);
+                    t.writeFile("OUTPUT4.TXT", output);
+                }
+                case 7 -> {
+                    float[] f = t.readFile("INPUT.TXT");
+                    float [] arr = t.insertionSort2(f);
+                    System.out.print("Input value: ");
+                    float inputValue = sc.nextFloat();
+                    int result = t.binarySearch(arr, 0, arr.length - 1, inputValue);
+                    System.out.println("Index of first element: " + result);
+                    t.writeFile("OUTPUT5.TXT", result);
+                }
+                default -> System.out.println("**Invalid choice. Try again.**");
             }
         }
     }
